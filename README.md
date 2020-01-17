26239cd621e397737ea305c98369448afb2d1614
Kane Mathers <kane@kanemathers.name>
2020-01-17 17:26:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# VLC GTK Theme Variant Plugin
+
+A VLC interface plugin that allows you to start VLC in your GTK theme's dark
+mode.
+
+![Image of VLC in Dark Mode](./vlc-dark-mode-screenshot.png)
+
+I'm running Gnome with the stock Adwaita (light) theme. I've configured
+VLC to run in Adwaita Dark mode.
+
+## Installation
+
+- Download `gtk-interface-variant.lua` to `~/.local/share/vlc/lua/intf/gtk-interface-variant.lua`
+- Start VLC with `vlc --extraintf luaintf --lua-intf gtk-interface-variant "$@"`
+
+There's probably a way to configure VLC to start with the extra interface
+without having to pass in these args but I haven't bothered to invest any
+time into figuring this out.
+
+I've gone the lazy route and created my own ~/bin/vlc wrapper with an
+accompanying /.local/share/applications/vlc.desktop file.
+
+Within VLC, you can then configure the window style to use your desired QT
+theme:
+
+Tools > Preferences > Interface > Force Window Style
+
---
+++ gtk-interface-variant.lua
@@ -0,0 +1,27 @@
+
+local window_title = "VLC media player"
+local variant = "dark"
+
+-- Need to wait a bit for VLC to initialise. It doesn't appear to set a title
+-- immediately. At least, from what I can gather by attempting an `xprop` call
+-- without this sleep.
+os.execute("sleep 0.3")
+
+-- First attempt is assuming VLC wasn't started with a file provided. When
+-- this happens, the window title defaults to "VLC media player"
+local exit_code = os.execute("xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT " .. variant .. " -name '" .. window_title .. "'")
+
+-- If this fails, we know the title is incorrect and we assume that's a
+-- result of VLC starting to play a file and the title being changed to
+-- "$file - VLC media player"
+if exit_code == nil then
+    local item
+
+    repeat
+        item = vlc.input.item()
+    until item
+
+    local wintitle = item:name() .. " - " .. window_title
+
+    os.execute("xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT " .. variant .." -name '" .. wintitle .. "'")
+end
